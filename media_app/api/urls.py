8bdabb82bfc7e21829e3a4058b4900e17d04e077
Maik G <maik.g@outlook.com>
2026-08-10 15:34:20 +0200

    Add HLS index view for video streaming with resolution support

--- media_app/api/urls.py
+++ media_app/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import VideoView
+from .views import VideoView, HlsIndexView
 
 
 
 urlpatterns = [
     path('video/', VideoView.as_view(), name='video'),
+    path('video/<int:movie_id>/<str:resolution>/index.m3u8', HlsIndexView.as_view(), name='hls_index'),
 
 ]
--- media_app/api/views.py
+++ media_app/api/views.py
@@ -4,6 +4,10 @@ from media_app.models import VideoModel
 from rest_framework.response import Response
 from .serializer import VideoSerializer
 from rest_framework.permissions import  IsAuthenticated
+from django.http import FileResponse
+from django.shortcuts import get_object_or_404
+from django.conf import settings
+from pathlib import Path
 
 
 
@@ -15,3 +19,17 @@ class VideoView(APIView):
         videos = VideoModel.objects.all()
         serializer = VideoSerializer(videos, many=True, context={'request': request})
         return Response(serializer.data, status=200)
+
+class HlsIndexView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, movie_id, resolution):
+
+        hls_index = get_object_or_404(VideoModel,pk=movie_id)
+        path = Path(settings.MEDIA_ROOT) /  'videos' / str(movie_id)  /str(resolution) / "index.m3u8"
+        if path.exists():
+            return FileResponse(open(path, 'rb'),content_type='application/vnd.apple.mpegurl')
+        else:
+            return Response(status=404)
+
