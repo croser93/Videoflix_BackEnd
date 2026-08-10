f0e6d749650e9e4dd97189b0822e85e9ad112fd1
Maik G <maik.g@outlook.com>
2026-08-10 12:26:55 +0200

    Add authentication to VideoView and enhance video retrieval tests

--- media_app/api/views.py
+++ media_app/api/views.py
@@ -3,13 +3,15 @@ from rest_framework.views import APIView
 from media_app.models import VideoModel
 from rest_framework.response import Response
 from .serializer import VideoSerializer
+from rest_framework.permissions import  IsAuthenticated
 
 
 
 class VideoView(APIView):
 
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         videos = VideoModel.objects.all()
         serializer = VideoSerializer(videos, many=True, context={'request': request})
-        print(serializer.data)
         return Response(serializer.data, status=200)
